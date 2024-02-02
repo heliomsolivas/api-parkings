6f0c1dfa36adf854172cce80e3f89b3ffdde21c4
Hélio M S Olivas <heliomsolivas@gmail.com>
2024-02-02 15:01:04 -0300

    Initial commit

---
+++ app/Models/Parking.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Parking extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['user_id', 'vehicle_id', 'zone_id', 'start_time', 'stop_time', 'total_price'];
+
+    protected $casts = [
+        'start_time' => 'datetime',
+        'stop_time' => 'datetime',
+    ];
+
+    /**Also, we add the $casts to the datetime columns, to be able to easily convert them later to whichever format we want. */
+}
